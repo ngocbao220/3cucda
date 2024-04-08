4e2cd248dcffebc8fb478816ec7b792cce2a6856
Cuongngucutee <23020339@vnu.edu.vn>
2024-04-08 20:01:49 +0700

    ppush

--- src/main/java/Controller/TranslateTextController.java
+++ src/main/java/Controller/TranslateTextController.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import Models.TranslateText;
+import com.jfoenix.controls.JFXButton;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
@@ -8,13 +9,53 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TranslateTextController implements Initializable {
+
+    @FXML
+    private JFXButton but_diction;
+
+    @FXML
+    private JFXButton but_game;
+
+    @FXML
+    private JFXButton but_home;
+
+    @FXML
+    private ImageView catcute;
+
+    @FXML
+    private AnchorPane home;
+
+    @FXML
+    private Pane inner_pane;
+
+    @FXML
+    private AnchorPane intro_diction;
+
+    @FXML
+    private Pane menu_inner_pane;
+
+    @FXML
+    private HBox root;
+
+    @FXML
+    private AnchorPane side_anPane;
+
+    @FXML
+    private JFXButton but_trans;
+
+    @FXML
+    private Pane translate;
 
     private String Language = "vi";
 
@@ -29,7 +70,7 @@ public class TranslateTextController implements Initializable {
     private TextArea TextOut;
 
     @FXML
-    private Button swap;
+    private JFXButton swap;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
