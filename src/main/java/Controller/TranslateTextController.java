eb819d16b1376982fc1a99b1811463cd96678e14
ngocbao220 <ngocbaotrinhtuan@gmail.com>
2024-04-08 11:08:40 +0700

    Add Models

---
+++ src/main/java/Controller/TranslateTextController.java
@@ -0,0 +1,65 @@
+package Controller;
+
+import Models.TranslateText;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyEvent;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TranslateTextController implements Initializable {
+
+    private String Language = "vi";
+
+    private int count = 0;
+
+    private Timeline timer;
+
+    @FXML
+    private TextArea TextIn;
+
+    @FXML
+    private TextArea TextOut;
+
+    @FXML
+    private Button swap;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        timer = new Timeline(new KeyFrame(Duration.millis(10),
+                event -> {
+                    String inputText = TextIn.getText();
+                    if (inputText.isBlank()) {
+                        TextOut.clear();
+                        return;
+                    }
+                    TextOut.setText(TranslateText.translate(inputText, Language));
+                }));
+        timer.setCycleCount(1);
+    }
+
+
+    @FXML
+    void swapLang(ActionEvent event) {
+        if (count %2 != 0 ) {
+            Language = "vi";
+            count++;
+        } else {
+            Language = "en";
+            count++;
+        }
+    }
+
+    @FXML
+    void translate(KeyEvent event) {
+        timer.play();
+    }
+
+}
