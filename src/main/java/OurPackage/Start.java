720a063735261f366eee5c6deff51aa3619c9c5e
ngocbao220 <ngocbaotrinhtuan@gmail.com>
2024-04-16 15:19:40 +0700

    Update

--- src/main/java/OurPackage/Start.java
+++ src/main/java/OurPackage/Start.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class Start extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Start.class.getResource("Screen/Home-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Start.class.getResource("Screen/Translate-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Our app");
         stage.setScene(scene);
