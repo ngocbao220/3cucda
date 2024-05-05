9324a7e916df6eeb03779886d5b3ac14dc6eac16
ngocbao220 <ngocbaotrinhtuan@gmail.com>
2024-05-05 22:21:19 +0700

    fix some bug in DictionaryController

--- src/main/java/OurPackage/Module/Constructor.java
+++ src/main/java/OurPackage/Module/Constructor.java
@@ -4,10 +4,5 @@ import java.util.*;
 
 
 public class Constructor {
-    //public static Map<String, String> HisWord = new HashMap<>();
-
     public static Set<String> MarkedWord = new HashSet<>();
-
-
-
 }
